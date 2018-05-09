d5c0b09c0fcdfd288ad4a067f18dfc9ff853c33c
LucaC94 <luca.c94.01@gmail.com>
2018-05-09 22:34:36 +0200

    Add files via upload

--- dataset_analysis/dataset_analysis.py
+++ dataset_analysis/dataset_analysis.py
@@ -1,10 +1,12 @@
 import multiprocessing
+import argparse
 import sys
 import os
 import re
 import pandas as pd
 import json
 import subprocess
+import pefile
 
 try:
 	import yara
@@ -13,28 +15,54 @@ except ImportError:
 	sys.exit()
 
 
-AVCLASS_path="/home/luca/Scrivania/tesi/avclass-master/avclass_labeler.py"
+
 err=open(os.devnull,"w")
 
 
 
-def compile_ruleset(ruleset_path):
+def compile_ruleset(ruleset_path, filterlist_path):
 	if(os.path.isdir(ruleset_path)==False):
 		print("Invalid directory given:", ruleset_path)
 		sys.exit()
 
+	if(filterlist_path!="" and os.path.isfile(filterlist_path)==False):
+		print("Invalid filtering file given:", filterlist_path)
+		sys.exit()
+
 	rules_dictionary={}
+
+	filter_list=[]
+
+	if(filterlist_path!=""):
+		filterlist_file = open(filterlist_path, "r")
+		filter_list_nl = filterlist_file.readlines()
+		for entry in filter_list_nl:
+			filter_list.append(entry.rstrip("\n"))
+
+	print(filter_list)
 
 	""" Creating the dictionary to import all rules at once. """
 	for dir, subdirs, rule_files in os.walk(ruleset_path):
 		for rule_file in rule_files:
 			try:
 				rule_file_w_path=os.path.join(dir, rule_file)
-				yara.compile(rule_file_w_path)
-				rules_namespace=rule_file
+				#print(rule_file_w_path)
+				to_filter=False
+				for entry in filter_list:
+					if(re.match("(?i).*"+entry+".*",rule_file_w_path) is not None):
+						print("File:", rule_file_w_path, "ignored as requested")
+						to_filter=True
+						break
+					if(to_filter==True):
+						break
+				if(to_filter==True):
+					continue
+				yara.compile(filepaths={rule_file_w_path : rule_file_w_path})
+				rules_namespace=rule_file_w_path
 				rules_dictionary[rules_namespace]=rule_file_w_path
 			except Exception as e:
-				print("Syntax error opening file: "+rule_file_w_path+" -skipping.")
+				#continue
+				print("Syntax error opening file: "+rule_file_w_path+" -skipping:",e)
 
 
 	print(rules_dictionary)
@@ -50,7 +78,7 @@ class Consumer(multiprocessing.Process):
 		self.yara_ruleset = yara_ruleset
 
 	def run(self):
-		df = pd.DataFrame(columns=['Exe name', 'Family','first_seen', 'last_seen', 'total', 'positives', 'ssdeep', 'yara_detected', 'yara_matching_rules' ])
+		df = pd.DataFrame(columns=['Exe name', 'Family','first_seen', 'last_seen', 'total', 'positives', 'ssdeep', 'imphash', 'yara_detected', 'yara_matching_rules' ])
 		#df = df.set_index('Exe name')
 		while True:
 			next_task = self.task_queue.get()
@@ -61,7 +89,7 @@ class Consumer(multiprocessing.Process):
 			dict=next_task(self.yara_ruleset)
 			df=df.append(dict, ignore_index=True)
 			self.task_queue.task_done()
-		print("Process ended successfully.")
+		#print("Process ended successfully.")
 		return
 
 class Task(object):
@@ -72,7 +100,6 @@ class Task(object):
 	def __call__(self, yara_ruleset):
 		report_path=os.path.join(self.dir_path,self.json_name)
 		json_file=open(report_path)
-		print("Loading file ", report_path)
 		data=json.load(json_file)
 		cmd=[AVCLASS_path, "-vt" , report_path]
 		mw_family=subprocess.check_output(cmd,stderr=err)
@@ -97,20 +124,55 @@ class Task(object):
 		dict['total'] = data['total']
 		dict['positives'] = data['positives']
 		dict['ssdeep'] = data['ssdeep']
+		dict['imphash'] = pefile.PE(exe_path).get_imphash()
 		dict['yara_detected'] = (len(matches)>0)
-		dict['yara_matching_rules'] = [_.rule for _ in matches]
+		dict['yara_matching_rules'] = [_.namespace+":"+_.rule for _ in matches]
 
 		return dict
 
 if __name__ == "__main__":
-	samples_path=sys.argv[1]
-	rules_path=sys.argv[2]
-	proc_num=int(sys.argv[3])
+
+	"""
+	usage: dataset_analysis.py -i INPUT_DIRECTORY -nc NUMBER_OF_CORES
+								[-f FILTERLIST] -y RULES_DIRECTORY -o OUTPUT_FILE_NAME
+								-c AVCLASS_DIRECTORY
+	"""
+	parser = argparse.ArgumentParser(description='DataSet Analysis Tool')
+	parser.add_argument('-n', '--number_of_cores',
+						required=True, help='Number of cores to use',default='1')
+	parser.add_argument('-i', '--inputdir', required=True,
+						help='Directory to read input VT reports from (json format only)')
+	parser.add_argument('-f', '--filterlist',
+						help='File with list of .yar not to check',required=False, default="")
+	parser.add_argument('-c', '--avclass_main', default="/home/luca/Scrivania/tesi/avclass-master",
+						help='AVCLASS main directory')
+	parser.add_argument('-o', '--output', required=True,
+						help='Output file name', default="./output.csv")
+	parser.add_argument('-y', '--yars', required=True,
+						 help='YARA rules')
+	args = parser.parse_args()
+	samples_path=args.inputdir
+	rules_path=args.yars
+	proc_num=int(args.number_of_cores)
+	filterlist=args.filterlist
+	global AVCLASS_path
+
+
+	if(os.path.isdir(args.avclass_main)==False):
+		print("The AVCLASS path is NOT a directory.")
+		sys.exit()
+
+	AVCLASS_path = os.path.join(args.avclass_main,"avclass_labeler.py")
+
+
+	if(os.path.isdir(samples_path)==False):
+		print("The samples directory is NOT a directory.")
+		sys.exit()
 
 	jobs=multiprocessing.JoinableQueue()
 	results = multiprocessing.Queue()
 
-	yara_ruleset=compile_ruleset(rules_path)
+	yara_ruleset=compile_ruleset(rules_path, filterlist)
 
 	consumers = [Consumer(jobs, results, yara_ruleset) for i in range(proc_num)]
 
@@ -125,21 +187,17 @@ if __name__ == "__main__":
 		jobs.put(None)
 
 	jobs.join()
-	print("Main awake.")
+	#print("Main awake.")
 
 	df_list = [results.get() for i in range(proc_num)]
 	pd_dataset= pd.concat(df_list)
 
 	yara_matches_count = len(pd_dataset[pd_dataset['yara_detected']==True])
 	print("Yara rules detected "+str(yara_matches_count)+" samples")
-	"""
-	packer_detection_count= len(pd_dataset[pd_dataset['possibly packed']==True])
-	print("Yara rules detected "+str(yara_matches_count)+" packers whereas the dumb rule detected "+str(packer_detection_count))
-	detection_hit = len(pd_dataset[(pd_dataset['yara packing detection']==True) & (pd_dataset['possibly packed']==True)])
-	detection_miss = len(pd_dataset[(pd_dataset['yara packing detection']==True) & (pd_dataset['possibly packed']==False)])
-	detection_false = len(pd_dataset[(pd_dataset['yara packing detection']==False) & (pd_dataset['possibly packed']==True)])
-	print("The dumb rule found "+str(detection_hit)+" also found by the yara rules")
-	print("The dumb rule couldn't find "+str(detection_miss)+" of those found by the yara rules")
-	print("The dumb rule found "+str(detection_false)+" false positives w.r.t. the yara rules")
-	"""
-	pd_dataset.to_csv(sys.argv[4])
+
+
+	if(os.path.isdir(args.output)==True):
+		output_file=os.path.join(args.output,"output.csv")
+	else:
+		output_file=args.output
+	pd_dataset.to_csv(output_file)
